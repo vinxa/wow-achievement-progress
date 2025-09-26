76080491769e0fb4addf6cf02f4cb76254d130c3
Gabby <69676454+vinxa@users.noreply.github.com>
2025-09-26 22:42:05 +0800

    add local browser storage for saving server and realm

--- static/serverSelection.js
+++ static/serverSelection.js
@@ -2,24 +2,34 @@
 /**
  * Loads the list of realms for the given region into the #serverSelect select.
  * @param {string} [region="us"] - The region to load realms for.
+ * @param {string}　[savedRealm=null] - optional saved realm.
+ * @param {string}　[savedServer=null] - optional saved server.
  */
-async function loadRealms(region = "us") {
-  const resp = await fetch(`/realms?region=${region}&t=${Date.now()}`);
-  const realms = await resp.json();
+async function loadRealms(region = "us", savedRealm = null, savedServer = null) {
+    const resp = await fetch(`/realms?region=${region}&t=${Date.now()}`);
+    const realms = await resp.json();
 
-  const select = $("#serverSelect");
-  select.empty();
+    const select = $("#serverSelect");
+    select.empty();
 
-  realms.forEach(r => {
-    const option = new Option(`${r.name}`,
-      r.name,
-      false,
-      false
-    );
-    select.append(option);
-  });
+    realms.forEach(r => {
+        const option = new Option(`${r.name}`,
+            r.name,
+            false,
+            false
+        );
+        select.append(option);
+    });
 
-  select.trigger("change.select2");
+    if (savedServer) {
+        select.val(savedServer).trigger("change");
+    }
+
+    if (savedRealm) {
+        select.val(savedRealm).trigger("change");
+    }
+
+    select.trigger("change.select2");
 }
 
 /**
@@ -28,18 +38,30 @@ async function loadRealms(region = "us") {
  * Sets up event listener for server select dropdown to focus search box when opened.
  */
 function setupServerSelection() {
-    $("#serverSelect").select2({
+    const serverSelect = $("#serverSelect");
+    const regionSelect = $("#regionSelect");
+
+    serverSelect.select2({
         placeholder: "Select or type a server",
         theme: "bootstrap4",
         width: "100%"
-        });
+    });
 
-        loadRealms("us");
-        $("#regionSelect").on("change", function () {
-            loadRealms(this.value);
-        });
+    const savedRegion = localStorage.getItem("region") || "us";
+    const savedServer = localStorage.getItem("server");
+    regionSelect.val(savedRegion);
+    loadRealms(savedRegion, savedServer);
 
-        $('#serverSelect').on('select2:open', function () {
+    regionSelect.on("change", function () {
+        loadRealms(this.value);
+        localStorage.setItem("region", this.value);
+    });
+
+    serverSelect.on("change", function () {
+        localStorage.setItem("server", this.value);
+    });
+
+    serverSelect.on('select2:open', function () {
         setTimeout(() => {
             let searchBox = document.querySelector('.select2-container--open .select2-search__field');
             if (searchBox) {
